e40ab51799a583cb9dc70f3e83d1f83e0803d3df
Jayson Garcia <jayfrostgarcia@gmail.com>
2020-04-11 23:19:20 +0800

    Setup for String resource via gen_lang dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:phcovid19tracker/about.dart';
-import './tracker.dart';
-import './about.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:phcovid19tracker/di/DependencyInjectors.dart';
+import 'package:phcovid19tracker/ui/Tracker/TrackerPage.dart';
 import './graphs.dart';
 import './history.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'ui/About/AboutView.dart';
+import 'package:phcovid19tracker/generated/i18n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
+  DependencyInjectors();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  final String APIHeaderRapidKey = FlutterConfig.get('API_KEY');
-  final String APIHeaderRapidHost = FlutterConfig.get('API_URL');
-  final String LocationKeyPH = FlutterConfig.get('API_COUNTRY_KEY');
-
+  final routes = {
+    "/about" : (BuildContext context) => new AboutPage(),
+    "/history" : (BuildContext context) => new CovidHistory(apiKey: FlutterConfig.get('API_KEY'), apiHost: FlutterConfig.get('API_URL'), locationKey: FlutterConfig.get('API_COUNTRY_KEY')),
+    "/statistics" : (BuildContext context) => new CovidStatistics(apiKey: FlutterConfig.get('API_KEY'), apiHost: FlutterConfig.get('API_URL'), locationKey: FlutterConfig.get('API_COUNTRY_KEY'))
+  };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: ThemeData( 
         primarySwatch: Colors.blue,
       ),
-      home: CovidTracker(apiKey: APIHeaderRapidKey, apiHost: APIHeaderRapidHost, locationKey: LocationKeyPH),
-      routes: <String, WidgetBuilder>{
-        "/about" : (BuildContext context) => new AboutPage(title: "About"),
-        "/history" : (BuildContext context) => new CovidHistory(apiKey: APIHeaderRapidKey, apiHost: APIHeaderRapidHost, locationKey: LocationKeyPH),
-        "/statistics" : (BuildContext context) => new CovidStatistics(apiKey: APIHeaderRapidKey, apiHost: APIHeaderRapidHost, locationKey: LocationKeyPH)
-      },
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: TrackerPage(),
+      routes: routes,
     );
   }
 }
